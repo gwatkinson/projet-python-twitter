3595d1772085b6a91b84d73687f2e4fe6e89fb80
Gabriel WATKINSON <56303915+gwatkinson@users.noreply.github.com>
2020-11-05 19:07:55 +0100

    Ajout du script de streaming (#9)
    * Ajout des requirements
    
    * Ajout de l'option 'path' dans SListener
    
    * Renomme 'scripts' en 'projet_python_twitter'
    
    * Ajout du '__init__'
        Pour pouvoir générer la documentation
    
    * Ajout des listes de mots dans un fichier à part
    
    * Création de la classe credentials_class
        Ajout de l'option timeout à start_stream.
        Remise en page et ajout des docstrings.

---
+++ projet_python_twitter/listes_mots.py
@@ -0,0 +1,49 @@
+# Liste des mots à tracker.
+liste_1 = [
+    "JoeBiden",
+    "realDonaldTrump",
+    "biden",
+    "trump",
+    "presidentialelection",
+    "presidential",
+    "election",
+    "electionnight",
+    "vote",
+    "iwillvote",
+    "america",
+    "govote",
+    "uselection",
+    "president",
+]
+
+liste_2 = [
+    "JoeBiden",
+    "realDonaldTrump",
+    "biden",
+    "trump",
+    "presidentialelection",
+    "electionnight",
+    "iwillvote",
+    "govote",
+    "uselection",
+]
+
+# Mathias
+liste_3 = [
+    "biden",
+    "trump",
+    "JoeBiden",
+    "realDonaldTrump",
+]
+
+# Wilfried
+liste_4 = ["iwillvote", "govote", "uselection", "vote"]
+
+# Gabriel
+liste_5 = [
+    "uselection",
+    "president",
+    "presidentialelection",
+    "presidential",
+    "electionnight",
+]
--- scripts/README.md
+++
@@ -1,3 +0,0 @@
-# Ce dossier contient les scripts python
-
-Contient les fonctions et classes **python** utilisés dans le notebook final
