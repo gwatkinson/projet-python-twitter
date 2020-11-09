26d20181f3421dc5738c617b21b831bb2d900675
Gabriel Watkinson <gabriel.watkinson@ensae.fr>
2020-11-09 20:00:21 +0100

    Ajout de tests pour streaming

---
+++ test/test_streaming.py
@@ -0,0 +1,78 @@
+import pytest
+import tweepy
+import projet_python_twitter.streaming as stream
+import projet_python_twitter.listes_mots as listes
+
+
+@pytest.fixture
+def file_credentials():
+    """Returns the dictionary in projet_python_twitter._credentials"""
+    try:
+        import projet_python_twitter._credentials
+
+        return projet_python_twitter._credentials.credentials
+    except ModuleNotFoundError as e:
+        print(
+            "Erreur : " + str(e),
+            "",
+            "Vérifier que '_credentials.py' existe bien et est dans le bon dossier ('projet_python_twitter/')",
+            sep="\n",
+        )
+        return None
+
+
+def test_file_cred(file_credentials):
+    """Test que les clés nécessaires sont bien dans '_credentials.py'"""
+    print(file_credentials)
+    assert all(
+        key in file_credentials
+        for key in ["consumer_key", "consumer_secret", "access_token", "bearer_token"]
+    )
+
+
+def test_credentials_type():
+    """Test qu'une erreur est levée quand credentials n'est pas un dictionnaire"""
+    with pytest.raises(stream.CredentialsType):
+        stream.credentials_class(credentials="XXXXXXXXX")
+
+
+def test_custom_cred_missing():
+    """Test qu'une erreur est levée si les credentials données ne sont pas complets"""
+    with pytest.raises(stream.MissingKey):
+        stream.credentials_class(
+            credentials={
+                "consumer_key": "",
+                "consumer_secret": "",
+                "access_token": "",
+                "az": 40,
+            }
+        )
+
+
+def test_custom_cred_type():
+    """Test qu'une erreur est levée si les credentials données ne sont pas complets"""
+    with pytest.raises(stream.CredentialsKeyType):
+        stream.credentials_class(
+            credentials={
+                "consumer_key": "",
+                "consumer_secret": 3,
+                "access_token": "",
+                "access_token_secret": "",
+            }
+        )
+
+
+# def test_init_cred():
+#     """Test si la classe s'initialise bien"""
+#     cred = stream.credentials_class(
+#         credentials={
+#             "consumer_key": "XXX",
+#             "consumer_secret": "XXX",
+#             "access_token": "XXX",
+#             "access_token_secret": "XXX",
+#         }
+#     )
+#     with pytest.raises(tweepy.TweepError):
+#         # Récupère la time_line des authentifiants
+#         # Renvoie une erreur s'ils sont incorrects
+#         cred.api.user_timeline(page=1)
