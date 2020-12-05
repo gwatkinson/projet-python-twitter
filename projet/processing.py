9d01ab743c9842515b753072fda163ac10a007c6
Gabriel Watkinson <gabriel.watkinson@ensae.fr>
2020-12-05 17:18:48 +0100

    Ajout d'une bartre de progrès
    Et des fonctions à compléter

--- projet/processing.py
+++ projet/processing.py
@@ -11,100 +11,93 @@ import glob
 # Erreurs du projet
 import projet.project_errors as errors
 
+# Affichage du progrès
+def progressBar(current, total, file=None, total_file=None, barLength=20):
+    percent = float(current) * 100 / total
+    arrow = "-" * int(percent / 100 * barLength - 1) + ">"
+    spaces = " " * (barLength - len(arrow) - 1)
+    prefix = (
+        f"File {file}/{total_file}"
+        if file is not None and total_file is not None
+        else "Progress"
+    )
+    print(f"{prefix}: [{arrow}{spaces}] {percent:.0f} %", end="\r")
+
 
 ## Converti et nettoie les tweets
-def folder_to_list_path(folder_path):
+def folder_to_path_list(folder_path):
     """
     Retourne la liste des fichiers `.json` dans le dossier donné.
 
     Args:
-        folder (str): Chemin du dossier.
+        folder_path (str): Chemin du dossier.
+        Ne pas terminer avec un ``/`` ou ``\``.
 
+    Examples:
+        folder_path("path/to/folder")
+        folder_path("path\\to\\folder")
+    
     Returns:
         list: Liste des fichiers `.json` dans le dossier.
     """
-    list_path = glob.glob(folder_path + "/*.json")
+    path_list = glob.glob(folder_path + "/*.json")
 
-    return list_path
+    return path_list
 
 
-def tweet_json_to_df(list_path):
+def tweet_json_to_df(path_list=None, folder=None):
     """
     Converti les fichiers json en dataframe pandas.
 
     Args:
-        list_path (list): 
-            Une liste de `str` qui contiennent les chemin vers les fichiers `.json`.
+        path_list (list, optional): 
+            Une liste des chemin vers les fichiers `.json`.
+        folder (str, optional): 
+            Le chemin du dossier qui contient les fichiers `.json`.
+            Ne pas terminer avec un ``/`` ou ``\``.
     
     Returns:
         pandas.dataframe: Dataframe pandas qui contient les tweets.
     """
+    assert path_list is not None or folder is not None, "Un argument est nécessaire"
+    assert path_list is None or (
+        path_list
+        and isinstance(path_list, list)
+        and all(isinstance(path, str) for path in path_list)
+    ), "'path_list' doit être une liste de strings"
+    assert folder is None or isinstance(
+        folder, str
+    ), "'folder' doit être une chaîne de caractères"
 
-    # Utiliser une liste de path plutot ?
-    # Puis faire une fonction qui renvoie une liste de tous les fichiers json dans un dosiier donné
+    if path_list is None:
+        path_list = folder_to_path_list(folder_path=folder)
 
-    assert (
-        isinstance(list_path, list) and len(list_path) > 1
-    ), "'list_path' doit être une liste"
-
-    # Liste des tweets nettoyés
-    tweets_list = []
-
-    # Nettoie et converti les tweets
     print("The file conversion started, this operation can take time")
-    for path in list_path:
+
+    # Contient la liste des tweets en json
+    tweets_list = []
+    file_total = len(path_list)
+    for i, path in enumerate(path_list):
         with open(path, "r") as fh:
             tweets_json = fh.read().split("\n")
-
-            # Itère sur chaque tweet
-            for tweet in tweets_json:
+            for j, tweet in enumerate(tweets_json):
+                tweet_total = len(tweets_json)
                 if tweet:
                     tweet_obj = json.loads(tweet)
-
-                    # Store the user screen name in 'user-screen_name'
-                    tweet_obj["user-screen_name"] = tweet_obj["user"]["screen_name"]
-
-                    # Check if this is a 140+ character tweet
-                    if "extended_tweet" in tweet_obj:
-                        # Store the extended tweet text in 'extended_tweet-full_text'
-                        tweet_obj["extended_tweet-full_text"] = tweet_obj[
-                            "extended_tweet"
-                        ]["full_text"]
-
-                    if "retweeted_status" in tweet_obj:
-                        # Store the retweet user screen name in 'retweeted_status-user-screen_name'
-                        tweet_obj["retweeted_status-user-screen_name"] = tweet_obj[
-                            "retweeted_status"
-                        ]["user"]["screen_name"]
-
-                        # Store the retweet text in 'retweeted_status-text'
-                        tweet_obj["retweeted_status-text"] = tweet_obj[
-                            "retweeted_status"
-                        ]["text"]
-
-                        if "extended_tweet" in tweet_obj["retweeted_status"]:
-                            tweet_obj[
-                                "retweeted_status-extended_tweet-full_text"
-                            ] = tweet_obj["retweeted_status"]["extended_tweet"][
-                                "full_text"
-                            ]
-
-                    if "quoted_status" in tweet_obj:
-                        tweet_obj["quoted_status-text"] = tweet_obj["quoted_status"][
-                            "text"
-                        ]
-
-                        if "extended_tweet" in tweet_obj["quoted_status"]:
-                            tweet_obj[
-                                "quoted_status-extended_tweet-full_text"
-                            ] = tweet_obj["quoted_status"]["extended_tweet"][
-                                "full_text"
-                            ]
-
                 tweets_list.append(tweet_obj)
+                progressBar(j, tweet_total, file=i + 1, total_file=file_total)
+        print("")
 
-    # Create a DataFrame from `tweets_list`
+    # Créer une DataFrame à partir de `tweets_list`
     df_tweets = pd.DataFrame(tweets_list)
+
+    # # Store the user screen name in 'user-screen_name'
+    # df_tweets["user-screen_name"] = df_tweets["user"]["screen_name"]
+
+    # # Check if this is a 140+ character tweet
+    # if "extended_tweet" in df_tweets:
+    #     # Store the extended tweet text in 'extended_tweet-full_text'
+    #     df_tweets["extended_tweet-full_text"] = df_tweets["extended_tweet"]["full_text"]
 
     # Drop all the text duplicate (Est ce qu'on a vraiment envie de supprimer les RT ?)
     # df_tweets = df_tweets.drop_duplicates(subset=["text"])
@@ -136,7 +129,37 @@ def select_time_range(df, start, end):
         pandas.dataframe: La dataframe filtrée.
 
     Examples:
-        select_time_range(df, "2020-11-03 08:15:00", "2020-11-03 22:30:00")
+        select_time_range(df, "2020-11-03 08:15:00", "2021-01-03 22:30:00")
     """
     start_time = pd.to_datetime(start, "%Y-%m-%d %H:%M:%S")
-    end_time = dt.strptime(end, "%Y-%m-%d %H:%M:%S")
+    end_time = pd.to_datetime(end, "%Y-%m-%d %H:%M:%S")
+
+    filtered_df = df[start_time < df["created_at"] < end_time]
+
+    return filtered_df
+
+
+def filter_df(df):
+    """
+    Fonction pour filtrer la df qui contient les tweets.
+
+    Il s'agit de selectionner les variables (donc garder que certaines colonnes) qui nous interresse 
+    (text, full text, les counts, la localisation, on supprime retweeted_status et quoted_status).
+    Il faut peut etre récuperer les counts via l'API (car on récupère les nouveaux tweets et ils n"ont pas encore de likes).
+    Il faudra peut etre utiliser des expr reg pour nettoyer les rt.
+    Mettre l'option de choisir le timespan.
+
+    Args:
+        df ([type]): [description]
+    """
+    pass
+
+
+def nlp(df):
+    """
+    Fonction pour ajouter la ou les colonnes de nlp (à l'aide de nltk ou TextBlob ou les deux)
+
+    Args:
+        df ([type]): [description]
+    """
+    pass
