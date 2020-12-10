e802e5d065e3f2991043fa15a8cadf1512213ee0
Gabriel Watkinson <gabriel.watkinson@ensae.fr>
2020-12-10 15:30:45 +0100

    Ajout de WrongColumnName
    Et changement de 'message' en 'msg'

--- projet/projet_utils.py
+++ projet/projet_utils.py
@@ -15,45 +15,43 @@ def progressBar(current, total, prefix=None, file=None, total_file=None, barLeng
     print(f"{prefix}: [{arrow}{spaces}] {percent:.0f} %", end="\r")
 
 
-## Les erreurs du projet
+### Les erreurs du projet
 
-
+## Erreurs de streaming.py
 class CredentialsType(Exception):
     """Erreur à lever si `credentials` n'est pas un dictionnaire."""
 
-    def __init__(self, type, message="'credentials' doit être un dictionnaire, pas un"):
+    def __init__(self, type, msg="'credentials' doit être un dictionnaire, pas un"):
         self.type = type
-        self.message = message
-        super().__init__(self.message)
+        self.msg = msg
+        super().__init__(self.msg)
 
     def __str__(self):
-        return f"{self.message} {str(self.type)}"
+        return f"{self.msg} {str(self.type)}"
 
 
 class MissingKey(Exception):
     """Erreur à lever s'il manque des clés dans le dictionnaire."""
 
-    def __init__(
-        self, missing_keys, message="Il manque des clés dans le dictionnaire."
-    ):
+    def __init__(self, missing_keys, msg="Il manque des clés dans le dictionnaire."):
         self.missing_keys = missing_keys
-        self.message = message
-        super().__init__(self.message)
+        self.msg = msg
+        super().__init__(self.msg)
 
     def __str__(self):
-        return f"{self.message} -> {str(self.missing_keys)}"
+        return f"{self.msg} -> {str(self.missing_keys)}"
 
 
 class CredentialsKeyType(Exception):
     """Erreur à lever si les clés ne sont pas des `str`."""
 
-    def __init__(self, wrong_keys, message="doivent être des strings."):
+    def __init__(self, wrong_keys, msg="doivent être des strings."):
         self.wrong_keys = wrong_keys
-        self.message = message
-        super().__init__(self.message)
+        self.msg = msg
+        super().__init__(self.msg)
 
     def __str__(self):
-        return f"{str(self.wrong_keys)} {self.message}"
+        return f"{str(self.wrong_keys)} {self.msg}"
 
 
 class CredentialsClassType(Exception):
@@ -62,23 +60,36 @@ class CredentialsClassType(Exception):
     def __init__(
         self,
         type,
-        message="'credentials' doit être une instance de 'credentials_class', pas un",
+        msg="'credentials' doit être une instance de 'credentials_class', pas un",
     ):
         self.type = type
-        self.message = message
-        super().__init__(self.message)
+        self.msg = msg
+        super().__init__(self.msg)
 
     def __str__(self):
-        return f"{self.message} {str(self.type)}"
+        return f"{self.msg} {str(self.type)}"
 
 
 class WordType(Exception):
     """Erreur à lever si les mots de la liste de mots à suivre ne sont pas des `str`."""
 
-    def __init__(self, wrong_words, message="doivent être des strings."):
+    def __init__(self, wrong_words, msg="doivent être des strings."):
         self.wrong_words = wrong_words
-        self.message = message
-        super().__init__(self.message)
+        self.msg = msg
+        super().__init__(self.msg)
 
     def __str__(self):
-        return f"{str(self.wrong_words)} {self.message}"
+        return f"{str(self.wrong_words)} {self.msg}"
+
+
+## Erreurs de processing.py
+class WrongColumnName(Exception):
+    """Erreur à lever si la variable donnée n'est pas dans la dataframe"""
+
+    def __init__(self, var, msg="n'est pas une colonne de la dataframe"):
+        self.var = var
+        self.msg = msg
+        super().__init__(self.msg)
+
+    def __str__(self):
+        return f"{str(self.msg)} {self.msg}"
