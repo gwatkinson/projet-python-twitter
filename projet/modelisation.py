e6492f8acc2f8f438866ced9f0bd1589e94853b9
Gabriel Watkinson <gabriel.watkinson@ensae.fr>
2020-12-13 18:51:06 +0100

    Update get_numeric

--- projet/modelisation.py
+++ projet/modelisation.py
@@ -16,8 +16,8 @@ import projet.projet_utils as utils
 
 
 # Fonctions pour le K-means
-def get_numeric(df):
-    return list(df.select_dtypes(include=[np.number, bool]))
+def get_numeric(df, drop_vars=["user-id"]):
+    return list(df.drop(drop_vars, axis=1).select_dtypes(include=[np.number, bool]))
 
 
 def standardize(df, vars):
@@ -38,27 +38,65 @@ def standardize(df, vars):
     return pd.DataFrame(scaled_features, columns=new_df.columns)
 
 
-def add_group(df, max_cluster=10):
+def KM(
+    df,
+    n_cluster=None,  # Prend la valeur optimal
+    label_var="kmlabel",
+    vars=None,
+    drop_vars=["user-id"],
+    n_init=10,
+    max_iter=300,
+    max_cluster=10,
+    random_state=40,
+    plot=False,
+):
+
+    if vars is None:
+        vars = get_numeric(df, drop_vars)
+    df2 = standardize(df, vars=vars)
+
     kmeans_kwargs = {
         "init": "random",
-        "n_init": 10,
-        "max_iter": 300,
-        "random_state": 42,
+        "n_init": n_init,
+        "max_iter": max_iter,
+        "random_state": random_state,
     }
 
     # A list holds the SSE values for each k
-    sse = []
-    for k in range(1, max_cluster + 1):
-        kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
-        kmeans.fit(df)
-        sse.append(kmeans.inertia_)
+    if n_cluster is None:
+        sse = []
+        for k in range(1, max_cluster + 1):
+            kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+            kmeans.fit(df2)
+            sse.append(kmeans.inertia_)
 
-    kl = KneeLocator(
-        range(1, max_cluster + 1), sse, curve="convex", direction="decreasing"
-    )
+        kl = KneeLocator(
+            range(1, max_cluster + 1), sse, curve="convex", direction="decreasing"
+        )
 
-    print(kl.elbow)
+        n_cluster = kl.elbow
 
-    kmeans = KMeans(n_clusters=kl.elbow, **kmeans_kwargs)
+        if plot:
+            plt.style.use("fivethirtyeight")
+            plt.plot(range(1, max_cluster + 1), sse, label="SSE")
+            plt.vlines(
+                n_cluster,
+                min(sse),
+                max(sse),
+                colors="r",
+                linestyles="dotted",
+                label="Nombre de clusters",
+            )
+            plt.xticks(range(1, max_cluster + 1))
+            plt.xlabel("Number of Clusters")
+            plt.ylabel("SSE")
+            plt.legend()
+            plt.show()
 
-    return kmeans.fit(df), sse
+    kmeans = KMeans(n_clusters=n_cluster, **kmeans_kwargs)
+
+    km = kmeans.fit(df2)
+
+    df[label_var] = km.labels_
+
+    return df
